0d260ea385e4dd2a40abb8abaeb3fdb8abc66c68
Javan Makhmali <javan@javan.us>
2015-03-31 11:39:26 -0600

    Console cleanup

--- lib/blade_runner/console.rb
+++ lib/blade_runner/console.rb
@@ -13,84 +13,20 @@ class BladeRunner
       EM.run do
         start_screen
         init_windows
-        EM.defer { handle_keys }
-
-        flash("Starting...")
+        init_tabs
+        handle_keys
 
         subscribe("/tests") do |details|
           process_test_result(details)
         end
 
         subscribe("/filewatcher") do
-          flash("Restarting...")
           publish("/commands", command: "start")
         end
       end
     end
 
     private
-      def start_screen
-        init_screen
-        start_color
-        noecho
-        curs_set(0)
-        @screen = stdscr
-        @screen.keypad(true)
-        refresh
-      end
-
-      def init_windows
-        @tabs = {}
-
-        init_pair(COLOR_GREEN,COLOR_GREEN,COLOR_BLACK)
-        @green = color_pair(COLOR_GREEN)
-
-        @y = 0
-        @x = 1
-
-        console_height = 2
-        @console = @screen.subwin(console_height, 0, @y, @x)
-        @y += console_height
-
-        @tab_height = 4
-        @tab_y = @y
-        @tab_x = @x
-        @y += @tab_height
-
-        results_height = @screen.maxy - @y
-        @results_window = @screen.subwin(results_height, 0, @y, @x)
-        @results_window.scrollok(true)
-        @y += results_height
-
-        @screen.refresh
-      end
-
-      def handle_keys
-        while ch = getch
-          case ch
-          when KEY_LEFT
-            nil
-          when KEY_RIGHT
-            if @tabs.size > 1
-              active_index = 0
-              @tabs.each_with_index do |(name, tab), index|
-                if tab.active
-                  active_index = index
-                end
-              end
-
-              if active_index < @tabs.size - 1
-                display_tab(@tabs.keys[active_index + 1])
-              else
-                display_tab(@tabs.keys[0])
-              end
-            end
-          else
-            flash("key: #{ch}")
-          end
-        end
-      end
-
       def process_test_result(details)
         details = OpenStruct.new(details)
         results = ""
@@ -119,44 +55,110 @@ class BladeRunner
         end
       end
 
-      def find_or_create_tab(name)
-        if @tabs[name]
-          @tabs[name]
-        else
-          @tabs[name] = OpenStruct.new(content: "")
-          if @tabs.size == 1
-            display_tab(name)
-          else
-            draw_tabs
+      def start_screen
+        init_screen
+        start_color
+        noecho
+        curs_set(0)
+        @screen = stdscr
+        @screen.keypad(true)
+        refresh
+      end
+
+      def init_windows
+        init_pair(COLOR_GREEN,COLOR_GREEN,COLOR_BLACK)
+        @green = color_pair(COLOR_GREEN)
+
+        y = 0
+        console_height = 2
+        @console = @screen.subwin(console_height, 0, y, 0)
+        y += console_height
+
+        @tab_height = 4
+        @tab_y = y
+        y += @tab_height
+
+        results_height = @screen.maxy - y
+        @results_window = @screen.subwin(results_height, 0, y, 1)
+        @results_window.scrollok(true)
+
+        @screen.refresh
+      end
+
+      def init_tabs
+        @tabs = []
+        runner.browsers.map(&:name).each { |n| create_tab(n) }
+        display_tab(@tabs.first) if @tabs.first
+      end
+
+      def handle_keys
+        EM.defer do
+          while ch = getch
+            case ch
+            when KEY_LEFT
+              change_tab(:previous)
+            when KEY_RIGHT
+              change_tab(:next)
+            end
           end
-          @tabs[name]
         end
       end
 
-      def draw_tabs
-        tab_x = @tab_x
+      def change_tab(direction = :next)
+        index = @tabs.index(@tabs.detect(&:active))
+        tabs = @tabs.rotate(index)
+        tab = direction == :next ? tabs[1] : tabs.last
+        display_tab(tab)
+      end
 
-        @tabs.each do |name, tab|
+      def find_or_create_tab(name)
+        if tab = find_tab(name)
+          tab
+        else
+          tab = create_tab(name)
+          draw_tabs
+          tab
+        end
+      end
+
+      def find_tab(name)
+        @tabs.detect { |t| t.name == name }
+      end
+
+      def create_tab(name)
+        tab = OpenStruct.new(name: name, content: "")
+        @tabs.push(tab)
+        tab
+      end
+
+      def draw_tabs
+        # Horizontal line
+        @screen.setpos(@tab_y + 2, 0)
+        @screen.addstr("═" * @screen.maxx)
+        @screen.refresh
+
+        tab_x = 1
+        @tabs.each do |tab|
           if tab.window
             tab.window.clear
             tab.window.close
           end
 
-          width = name.length + 4
+          width = tab.name.length + 4
           window = @screen.subwin(@tab_height, width, @tab_y, tab_x)
 
           if tab.active
-            inner_width = name.length + 2
+            inner_width = tab.name.length + 2
             window.addstr "╔" + "═" * inner_width + "╗"
             window.addstr "║"
             window.attron(@green)
-            window.addstr(" #{name} ")
+            window.addstr(" #{tab.name} ")
             window.attroff(@green)
             window.addstr                           "║"
             window.addstr "╝" + " " * inner_width + "╚"
           else
             window.addstr "\n"
-            window.addstr "  #{name}  "
+            window.addstr "  #{tab.name}  "
             window.addstr "═" * width
           end
 
@@ -166,28 +168,14 @@ class BladeRunner
         end
       end
 
-      def display_tab(name)
-        flash(name)
-
-        tab = @tabs[name]
-        @tabs.values.each { |t| t.active = false }
+      def display_tab(tab)
+        @tabs.each { |t| t.active = false }
         tab.active = true
         draw_tabs
 
         @results_window.clear
         @results_window.addstr(tab.content)
         @results_window.refresh
-      end
-
-      def flash(message)
-        @console.clear
-        @console.addstr(message)
-        @console.refresh
-
-        EM.add_timer(2) do
-          @console.clear
-          @console.refresh
-        end
       end
   end
 end
