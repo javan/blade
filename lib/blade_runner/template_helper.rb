4b89bd5a6ada05460b8bbf077932fe401d825d30
Javan Makhmali <javan@javan.us>
2015-06-21 16:35:24 -0400

    Load remote head and body html partials

--- lib/blade_runner/template_helper.rb
+++ lib/blade_runner/template_helper.rb
@@ -1,16 +1,24 @@
 module BladeRunner::TemplateHelper
   def blade_runner_head
-    html = []
-    html << %Q(<meta name="websocket" content="#{BR::Server.websocket_url}">)
-    html << %Q(<script src="/blade.js"></script>)
-    html.join("\n")
+    [].tap do |html|
+      html << %Q(<meta name="websocket" content="#{BR::Server.websocket_url}">)
+      html << %Q(<script src="/blade.js"></script>)
+
+      if asset = BR::Assets.environment['_test_head.html']
+        html << asset.to_s
+      end
+    end.join("\n")
   end
 
   def blade_runner_body
-    html = []
-    BR::Assets.logical_paths(:js).each do |path|
-      html << %Q(<script src="/#{path}"></script>)
-    end
-    html.join("\n")
+    [].tap do |html|
+      BR::Assets.logical_paths(:js).each do |path|
+        html << %Q(<script src="/#{path}"></script>)
+      end
+
+      if asset = BR::Assets.environment['_test_body.html']
+        html << asset.to_s
+      end
+    end.join("\n")
   end
 end
